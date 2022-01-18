137d1bd61626487c6bdaae831a24b3e14f782dae
Stéphane Bressani <stephane@bressani.dev>
2022-01-18 19:41:16 +0100

    Initial Commit

---
+++ astrologiePourIPhone11/astrologiePourIPhone11App.swift
@@ -0,0 +1,17 @@
+//
+//  astrologiePourIPhone11App.swift
+//  astrologiePourIPhone11
+//
+//  Created by Stéphane on 18.01.22.
+//
+
+import SwiftUI
+
+@main
+struct astrologiePourIPhone11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ astrologiePourIPhone11UITests/astrologiePourIPhone11UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  astrologiePourIPhone11UITestsLaunchTests.swift
+//  astrologiePourIPhone11UITests
+//
+//  Created by Stéphane on 18.01.22.
+//
+
+import XCTest
+
+class astrologiePourIPhone11UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
