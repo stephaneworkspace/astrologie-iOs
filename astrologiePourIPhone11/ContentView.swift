c33cda40b8164cfba5c8fcd8f7d0f78cad2c62a0
Stéphane Bressani <stephane@bressani.dev>
2022-01-21 15:03:29 +0100

    input

--- astrologiePourIPhone11/ContentView.swift
+++ astrologiePourIPhone11/ContentView.swift
@@ -7,19 +7,67 @@
 
 import SwiftUI
 
+private func loadValue(@State selectedDate: Date, @State selectedDateTransit: Date) -> Swe.Chart {
+    var chartDefault: Swe.Chart = loadDefaultValue()
+    //
+    var nLng = chartDefault.nLng
+    var tLng = chartDefault.tLng
+    var nLat = chartDefault.nLat
+    var tLat = chartDefault.tLat
+    var nTimeZone = chartDefault.nTimeZone
+    var tTimeZone = chartDefault.tTimeZone
+    var dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "YY"
+    var nYear = Int32(dateFormatter.string(from: selectedDate)) ?? chartDefault.nYear
+    var tYear = Int32(dateFormatter.string(from: selectedDateTransit)) ?? chartDefault.tYear
+    dateFormatter.dateFormat = "MM"
+    var nMonth = Int32(dateFormatter.string(from: selectedDate)) ?? chartDefault.nMonth
+    var tMonth = Int32(dateFormatter.string(from: selectedDateTransit)) ?? chartDefault.tMonth
+    dateFormatter.dateFormat = "dd"
+    var nDay = Int32(dateFormatter.string(from: selectedDate)) ?? chartDefault.nDay
+    var tDay = Int32(dateFormatter.string(from: selectedDateTransit)) ?? chartDefault.tDay
+    var nHour = chartDefault.nHour
+    var tHour = chartDefault.tHour
+    var nMin = chartDefault.nMin
+    var tMin = chartDefault.tMin
+    //
+    return Swe.Chart(
+            nLat: nLat,
+            nLng: nLng,
+            nTimeZone: nTimeZone,
+            nYear: nYear,
+            nMonth: nMonth,
+            nDay: nDay,
+            nHour: nHour,
+            nMin: nMin,
+            tLat: tLat,
+            tLng: tLng,
+            tTimeZone: tTimeZone,
+            tYear: tYear,
+            tMonth: tMonth,
+            tDay: tDay,
+            tHour: tHour,
+            tMin: tMin)
+}
+
 struct ContentView: View {
-    var chart: Swe.Chart = load_default_value()
+    @State var selectedDate = Date()
+    @State var selectedDateTransit = Date()
+    var chartDefault: Swe.Chart = loadDefaultValue()
     var body: some View {
         ZStack {
             VStack {
                 Text("Astrologie").padding()
-                ChartView(swe: Swe(chart: chart))
+                DatePicker("Date de naissance", selection: $selectedDate, displayedComponents: .date)
+                DatePicker("Transit", selection: $selectedDateTransit, displayedComponents: .date)
+                ChartView(swe: Swe(chart: loadDefaultValue()))
+                ChartView(swe: Swe(chart: loadValue(selectedDate: selectedDate, selectedDateTransit: selectedDateTransit)))
             }
         }
     }
 }
 
-private func load_default_value() -> Swe.Chart {
+private func loadDefaultValue() -> Swe.Chart {
     var decode: Swe.Chart = Swe.Chart.init(
             nLat: 46.12,
             nLng: 6.09,
